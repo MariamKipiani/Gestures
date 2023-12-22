40f481517c868af0fcc5034f8bd058cf58e7cbbe
MariamKipiani <145837678+MariamKipiani@users.noreply.github.com>
2023-12-22 19:12:18 +0400

    Initial Commit

---
+++ Gestures/GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesApp.swift
+//  Gestures
+//
+//  Created by user on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
